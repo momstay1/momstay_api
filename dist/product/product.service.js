fb790e452ff9d8f1d7ad5a8b513d28d89ed4a5fa
shjeon <shjeon@reconers.com>
2023-02-03 14:25:09 +0900

    npm run build

--- dist/product/product.service.js
+++ dist/product/product.service.js
@@ -68,18 +68,16 @@ let ProductService = class ProductService {
         const product = await this.productRepository.save(productEntity);
         const fileIdx = (0, lodash_1.get)(createProductDto, 'filesIdx', '');
         let fileIdxs = [];
-        if (fileIdx) {
-            try {
-                const productFileIdxs = (0, lodash_1.map)(await this.fileService.findCategory(["lodgingDetailImg", "mealsImg"], "" + product['idx']), (o) => "" + o.file_idx);
-                fileIdxs = fileIdx.split(",");
-                const delFileIdxs = productFileIdxs.filter(o => !fileIdxs.includes(o));
-                if (delFileIdxs.length > 0) {
-                    await this.fileService.removes(delFileIdxs);
-                }
+        try {
+            const productFileIdxs = (0, lodash_1.map)(await this.fileService.findCategory(["lodgingDetailImg", "mealsImg"], "" + product['idx']), (o) => "" + o.file_idx);
+            fileIdxs = fileIdx.split(",");
+            const delFileIdxs = productFileIdxs.filter(o => !fileIdxs.includes(o));
+            if (delFileIdxs.length > 0) {
+                await this.fileService.removes(delFileIdxs);
             }
-            catch (error) {
-                console.log({ error });
-            }
+        }
+        catch (error) {
+            console.log({ error });
         }
         let new_file;
         if (!(0, lodash_1.isEmpty)(files)) {
