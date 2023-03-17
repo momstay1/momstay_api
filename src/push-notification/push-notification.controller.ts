5b577f7fe5b0c385d081823c9d572c5e809a9976
shjeon <shjeon@reconers.com>
2023-03-17 17:02:05 +0900

    [ADD] 앱 푸시 테스트 api 추가

--- src/push-notification/push-notification.controller.ts
+++ src/push-notification/push-notification.controller.ts
@@ -18,20 +18,63 @@ export class PushNotificationController {
   }
 
   @Get('test')
-  async test() {
+  @ApiOperation({
+    summary: 'app push test',
+    description: 'topic, token 둘중 하나만 설정 필요'
+  })
+  @ApiQuery({
+    name: "topic",
+    description: 'topic전달 값 token 빈값으로 설정',
+    required: false
+  })
+  @ApiQuery({
+    name: "token",
+    description: 'token전달 값 topic 빈값으로 설정',
+    required: false
+  })
+  @ApiQuery({
+    name: "title",
+    description: '앱 푸시 제목',
+    required: false
+  })
+  @ApiQuery({
+    name: "body",
+    description: '앱 푸시 설명',
+    required: false
+  })
+  async test(
+    @Query('topic') topic: string,
+    @Query('token') token: string,
+    @Query('title') title: string,
+    @Query('body') body: string,
+  ) {
     const target = {
       // token: 'cJfclSYOSzOS86FH2TQHOz:APA91bGHhZpeZ3sg90Q6RnuPTCtWwJ_y0o3nVyh-xPCOQXYlj4XZb_HLHyp7gQFEY56YcIQPmZjUwSVNfZuOienzIkc2KYZoIJQyYToH4ZJ6T6EPZE_EMcZFNEtE4Z5riCpmazJRjJxE',
-      token: '5BAA98BFE537F9A4577F2899A647F122A0C9436F1421024A678E3211C93672C2',
-      // token: '',
-      // topic: 'all',
-      topic: '',
+      // token: '5BAA98BFE537F9A4577F2899A647F122A0C9436F1421024A678E3211C93672C2',
+      token: '',
+      topic: 'marketing',
+      // topic: '',
     }
     const notifications = {
       title: '앱 푸시 테스트',
       body: '앱 푸시 테스트 입니다.',
-      data: {}
+      // data: {}
     };
-    return await this.pushNotificationService.sendPush(target, notifications);
+    if (topic) {
+      target['topic'] = topic;
+      target['token'] = '';
+    }
+    if (token) {
+      target['token'] = token;
+      target['topic'] = '';
+    }
+    if (title) {
+      notifications['title'] = title;
+    }
+    if (body) {
+      notifications['body'] = body;
+    }
+    await this.pushNotificationService.sendPush(target, notifications);
   }
 
   @Get()
