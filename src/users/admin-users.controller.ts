1eb9fa392881e7859319b70fb828cbced2bac01a
shjeon <shjeon@reconers.com>
2023-04-18 15:53:39 +0900

    충돌 수정

--- src/users/admin-users.controller.ts
+++ src/users/admin-users.controller.ts
@@ -182,17 +182,6 @@ export class AdminUsersController {
   }
 
   // 회원 정보 가져오기
-  @Get(':idx')
-  @Auth(['root', 'admin'])
-  @ApiBearerAuth()
-  @ApiOperation({ summary: '회원 아이디 조회(index) API' })
-  @ApiOkResponse({ type: ProfileUserDto })
-  async findIdx(@Param('idx') idx: string) {
-    const data = await this.usersService.findIdx(+idx);
-    return data;
-  }
-
-  // 회원 정보 가져오기
   @Get(':id')
   @Auth(['root', 'admin'])
   @ApiBearerAuth()
