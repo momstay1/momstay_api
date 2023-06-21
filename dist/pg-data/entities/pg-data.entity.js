8e82d8db6012abc420d6cb0931fa8fc5658ca18b
shjeon <shjeon@reconers.com>
2023-06-21 17:37:05 +0900

    npm run build

--- dist/pg-data/entities/pg-data.entity.js
+++ dist/pg-data/entities/pg-data.entity.js
@@ -19,7 +19,7 @@ __decorate([
     __metadata("design:type", Number)
 ], PgDataEntity.prototype, "idx", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "productCode", void 0);
 __decorate([
@@ -27,55 +27,55 @@ __decorate([
     __metadata("design:type", Number)
 ], PgDataEntity.prototype, "amount", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "imp_uid", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "merchant_uid", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "pay_method", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "channel", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "pg_provider", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "pg_tid", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "pg_id", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "escrow", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "apply_num", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "bank_code", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "bank_name", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "card_code", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "card_name", void 0);
 __decorate([
@@ -87,23 +87,23 @@ __decorate([
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "card_number", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "card_type", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "vbank_code", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "vbank_name", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "vbank_num", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "vbank_holder", void 0);
 __decorate([
@@ -115,7 +115,7 @@ __decorate([
     __metadata("design:type", Number)
 ], PgDataEntity.prototype, "vbank_issued_at", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "name", void 0);
 __decorate([
@@ -123,27 +123,27 @@ __decorate([
     __metadata("design:type", Number)
 ], PgDataEntity.prototype, "cancel_amount", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "currency", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "buyer_name", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "buyer_email", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "buyer_tel", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "buyer_addr", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "buyer_postcode", void 0);
 __decorate([
@@ -151,11 +151,11 @@ __decorate([
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "custom_data", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "user_agent", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "status", void 0);
 __decorate([
@@ -175,15 +175,15 @@ __decorate([
     __metadata("design:type", Number)
 ], PgDataEntity.prototype, "cancelled_at", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "fail_reason", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "cancel_reason", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "receipt_url", void 0);
 __decorate([
@@ -195,15 +195,15 @@ __decorate([
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "cancel_receipt_urls", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "cash_receipt_issued", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "customer_uid", void 0);
 __decorate([
-    (0, typeorm_1.Column)({ length: 255, default: '' }),
+    (0, typeorm_1.Column)({ length: 255, default: '', nullable: true }),
     __metadata("design:type", String)
 ], PgDataEntity.prototype, "customer_uid_usage", void 0);
 __decorate([
