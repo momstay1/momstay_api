72e2bd028349dce1d6864a81520d5289f4d81344
shjeon <shjeon@reconers.com>
2023-07-14 15:54:51 +0900

    npm run build

--- dist/file/file.service.js
+++ dist/file/file.service.js
@@ -325,8 +325,7 @@ let FileService = class FileService {
     async sharpFile(file) {
         console.log('이미지 용량 및 사이즈 축소');
         console.log('이미지 경로: ', file.file_full_path);
-        const fileBuffer = fs.readFileSync(file.file_full_path);
-        const image = sharp(fileBuffer, { failOnError: false });
+        const image = sharp(file.file_full_path, { failOnError: false });
         const { format, width, height } = await image.metadata();
         if (width >= 1200) {
             console.log('이미지 넓이: ', { width });
