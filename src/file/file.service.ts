a9ee99db804c6db0caa34b793a0c0ca9685bd412
shjeon <shjeon@reconers.com>
2023-07-14 15:38:33 +0900

    [FIX] 이미지에 워터마크 추가시 원하는 위치에 추가 안되는 오류 수정
    이미지정보중 EXIF에 이미지의 위, 아래 위치 정보를 보존하는 경우가 있는데
    (핸드폰으로 촬영한 이미지의 경우 거꾸로 촬영해도 자동으로 위아래를 보정해줌)
    백엔드에서 리사이즈 및 워터마크 처리시 해당 정보로 인해 이미지가 돌아가는 현상이 발생
    이미지 처리시에 이미지가 자동으로 회전되지 않도록 수정
    
    리사이즈의 경우 withMetadata() 로 EXIF 정보 보존하여 회전 불가능하도록 처리
    워터마크의 경우 withMetadata() 로 EXIF 정보 보존해도 워터마크 이미지가 회전된 이미지로 인식하여 원하는 위치에 추가 불가
                         rotate(null) 사용하여 이미지가 회전 되지 않는 방법으로 원하는 위치에 워터마크 추가되도록 수정

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -401,8 +401,9 @@ export class FileService {
   async sharpFile(file) {
     console.log('이미지 용량 및 사이즈 축소');
     console.log('이미지 경로: ', file.file_full_path);
-    // const fileBuffer = fs.readFileSync(file.file_full_path);
-    const image = sharp(file.file_full_path, { failOnError: false });
+    const fileBuffer = fs.readFileSync(file.file_full_path);
+    // const image = sharp(file.file_full_path, { failOnError: false });
+    const image = sharp(fileBuffer, { failOnError: false });
     const { format, width, height } = await image.metadata();
 
     if (width >= 1200) {
@@ -431,20 +432,21 @@ export class FileService {
     const image = sharp(file_data.file_full_path, { failOnError: false });
     const { width, height } = await image.metadata();
 
-    // const watermark_img_local_path = './src/file/watermark/watermark.png';
+    const watermark_img_local_path = './src/file/watermark/watermark.png';
     const watermark_img_path = '/home/momstay_api/wwwhost/src/file/watermark/watermark.png';
     // const watermarkFileBuffer = fs.readFileSync(watermark_img_path);
-    const watermark = sharp(watermark_img_path, { failOnError: false });
+    const watermark = sharp(watermark_img_local_path, { failOnError: false });
+    // const watermark = sharp(watermark_img_path, { failOnError: false });
     const multipleNum = width < height ? 3 : 4;
-    console.log({ multipleNum });
     // 워터마크 이미지 리사이즈
     console.log('워터마크 이미지 리사이즈');
     await watermark.resize(+(width / multipleNum).toFixed(), null, { fit: 'contain' });
     const watermarkBuffer = await watermark.toBuffer();
 
     console.log('이미지에 워터마크 추가');
-    const watermarked = await image
-      .withMetadata()
+
+    await image
+      .rotate(null)
       .composite([{
         input: watermarkBuffer,
         gravity: 'southeast', // 워터마크 위치 오른쪽 밑
@@ -453,7 +455,6 @@ export class FileService {
         console.log(`워터마크된 이미지 info : ${JSON.stringify(info, null, 2)}`);
         console.log(`워터마크된 이미지 err : ${JSON.stringify(err, null, 2)}`);
       })
-    // .toBuffer()
     console.log('이미지에 워터마크 추가 완료');
   }
 
